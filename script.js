127cc05c98a0b38f6c6244d522255116f27e35d7
Hossain Uddin Ahamad <hossainuddinahamad@gmail.com>
2026-06-19 02:27:29 +0600

    optimized

--- script.js
+++ script.js
@@ -412,28 +412,30 @@ if (document.readyState === "loading") {
   initCodeVerse();
 }
 
-$(document).ready(function () {
-  $(".testimonial").slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    dots: false,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
+if (typeof $ !== "undefined" && typeof $.fn.slick !== "undefined") {
+  $(document).ready(function () {
+    $(".testimonial").slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows: false,
+      dots: false,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+          },
         },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
+        {
+          breakpoint: 640,
+          settings: {
+            slidesToShow: 1,
+          },
         },
-      },
-    ],
+      ],
+    });
   });
-});
+}
